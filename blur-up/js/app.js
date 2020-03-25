c8198c54fbdd67743c80ce9149f4c8fbcbbde028
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-03-25 12:59:11 +0100

    add description / title

--- blur-up/js/app.js
+++ blur-up/js/app.js
@@ -1,15 +1,14 @@
-'user strict';
+"user strict";
 
 (async () => {
- const app = document.querySelector('#app main');
+    const app = document.querySelector("#app main");
 
-  const result = await fetch('/data/spacex.json');
-  const data = await result.json();
-  
-  
-  const cards = data.map(item => {
-    const constructor = document.createElement('div');
-    constructor.innerHTML = `
+    const result = await fetch("/data/spacex.json");
+    const data = await result.json();
+
+    const cards = data.map((item) => {
+        const constructor = document.createElement("div");
+        constructor.innerHTML = `
       <section class="card">
         <header>
         <figure>
@@ -23,59 +22,59 @@
         </main>
       </section>
     `;
-    const card = constructor.querySelector('.card');
-    initCard(card, item);
+        const card = constructor.querySelector(".card");
+        initCard(card, item);
 
-    app.appendChild(card);
-    return card;
-  });
-
-  // setTimeout(() => {
-  //   cards.forEach(card => {
-  //     const cover = card.querySelector('img');
-  //     cover.src = cover.dataset.src;
-
-  //     const placeholder = card.querySelector('.placeholder');
-  //     placeholder.classList.add('fade');
-  //   });
-  // }, 2000);
-
-  /**
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-   */
-  const options = {
-    rootMarging : '0px 0px 0px 0px'
-  };
-
-  const callback = (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const image = entry.target;
-        image.src = image.dataset.src;
-
-        image.onload = () => {
-          image.parentNode.querySelector('.placeholder').classList.add('fade');
-        }
-      }
+        app.appendChild(card);
+        return card;
     });
-  }
 
-  const io = new IntersectionObserver(callback, options);
-  cards.forEach(card => {
-    const image = card.querySelector('img');
-    io.observe(image);
-  });
+    // setTimeout(() => {
+    //   cards.forEach(card => {
+    //     const cover = card.querySelector('img');
+    //     cover.src = cover.dataset.src;
+
+    //     const placeholder = card.querySelector('.placeholder');
+    //     placeholder.classList.add('fade');
+    //   });
+    // }, 2000);
+
+    /**
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+     */
+    const options = {
+        rootMarging: "0px 0px 0px 0px"
+    };
+
+    const callback = (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const image = entry.target;
+                image.src = image.dataset.src;
+
+                image.onload = () => {
+                    image.parentNode
+                        .querySelector(".placeholder")
+                        .classList.add("fade");
+                };
+            }
+        });
+    };
+
+    const io = new IntersectionObserver(callback, options);
+    cards.forEach((card) => {
+        const image = card.querySelector("img");
+        io.observe(image);
+    });
 })();
 
-
 function initCard(card, data) {
-  const placeholder = card.querySelector('.placeholder');
-  placeholder.style.cssText = `background-image: url(${data.placeholder})`;
+    const placeholder = card.querySelector(".placeholder");
+    placeholder.style.cssText = `background-image: url(${data.placeholder})`;
 
-  const image = card.querySelector('img');
-  image.dataset.src = data.image;
-  image.alt = data.content.title;
-
-  // ...
-
+    const image = card.querySelector("img");
+    image.dataset.src = data.image;
+    image.alt = data.content.title;
+    image.description = data.content.description;
+    image.title = data.content.title;
 }
